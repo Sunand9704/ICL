2d328a765e698c7a87539bdc1fb32d1716bce279
Suand9704 <sunandvemavarapu@gmail.com>
2025-08-08 16:24:13 +0530

    Fix: correct toaster import path

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Checkout from "./pages/Checkout";
 import Account from "./pages/Account";
 import NotFound from "./pages/NotFound";
 
+
 const queryClient = new QueryClient();
 
 const App = () => (
